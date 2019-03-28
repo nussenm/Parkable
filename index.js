154d2b6bce57a562fcdb797eb800031f26231541
Matt Nussen <mnussen1@kent.edu>
2019-03-28 14:43:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var awsIot = require('aws-iot-device-sdk');
+var name;
 
 var thingShadows = awsIot.thingShadow({
    keyPath: './certs/e7b2268d70-private.pem.key',
@@ -9,35 +10,18 @@ var thingShadows = awsIot.thingShadow({
    host: 'a1azsp5amzvmuf-ats.iot.us-east-2.amazonaws.com'
 });
 
-var device = awsIot.device({
-   keyPath: './certs/e7b2268d70-private.pem.key',
-  certPath: './certs/e7b2268d70-certificate.pem.crt',
-    caPath: './certs/AmazonRootCA1.pem',
-    clientId: 'arn:aws:iot:us-east-2:205839533183:thing/RaspberryPi',
-      host: 'a1azsp5amzvmuf-ats.iot.us-east-2.amazonaws.com'
-});
 
-//
-// Device is an instance returned by mqtt.Client(), see mqtt.js for full
-// documentation.
-//
-device
-  .on('connect', function() {
-    console.log('connect');
-    device.subscribe('$aws/things/RaspberryPi/shadow/get');
-    device.publish('topic_2', JSON.stringify({ test_data: 1}));
-  });
-
-
-
-
-//
-// This will print a console message when the connection to AWS IoT
-// completes.
-//
+//Set the name of the device we want, then execute these blocks
+//For example, invidual names yadda yadda
+name = 'RaspberryPi';
 thingShadows.on('connect', function() {
-   console.log('Connected to AWS IoT');
+    thingShadows.register(name, {}, function() {
+       thingShadows.get(name);
+    });
 });
-thingShadows.register('arn:aws:iot:us-east-2:205839533183:thing/RaspberryPi');
-var shadow = thingShadows.get('arn:aws:iot:us-east-2:205839533183:thing/RaspberryPi');
-console.log(shadow);
+
+thingShadows.on('status',  function(name, stat, clientToken, stateObject) {
+  console.log('received '+stat+' on '+name+': '+JSON.stringify(stateObject));
+  var carState = stateObject.state.reported.CAR; //SUCCESSFULLY GETS CAR VALUE
+  console.log(carState);
+});
